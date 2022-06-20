fd3e0de71a3d9324bc7824f864b7b83f309b6269
leegihe95 <leegihe95@gmail.com>
2022-06-20 11:24:47 +0900

    Added style sheet

--- minichat.php
+++ minichat.php
@@ -4,12 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Minichat</title>
-    <style>
-        form {
-            text-align: center;
-        }
-    </style>
 </head>
 <body>
     <form action="minichat_post.php" method="POST">
